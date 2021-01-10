be32b079028c058cb1ed8e5c2aa013d56d21ecb9
Kris <kunxinlin@gmail.com>
2021-01-10 02:15:05 -0500

    asdasd

--- Complete 2020 Web Development Bootcamp/Web Development/todolist-v1/app.js
+++ Complete 2020 Web Development Bootcamp/Web Development/todolist-v1/app.js
@@ -3,10 +3,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use("view engine", "ejs");
+
+
+//assumes theres a views directory that contains an ejs page
 app.get("/", function(req, res){
-  res.send("Hello");
+
+  var today = new Date();
+  var currentDay = today.getDate();
+  var day = "";
+
+  if(currentDay === 6 || currentDay === 0){
+    day = "Weekend";
+  } else {
+    day = "Weekday";
+  }
+
+  //render assumes theres a view directory with an ejs file called list
+  //the key has to be the variable in the ejs file
+  //the value is whats being pass to it, which is day in this case
+  //usually use same variable, ex day: day but for this example to clarify
+  res.render("list", {kindOfDay: day});
+
 });
 
 app.listen(3000, function(){
-  console.log("Server started on port 3000")
-})
+  console.log("Server started on port 3000");
+});
